469c829a883b11983071203f1673dbf4d0220a4a
tifufu <dematasjoshua@hotmail.com>
2025-02-08 18:43:23 +0100

    feat: added flags for store and db

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"fmt"
+
 	"github.com/charmbracelet/log"
 	"github.com/spf13/viper"
 )
@@ -8,4 +10,29 @@ import (
 type CLI struct {
 	Logger *log.Logger
 	Config *viper.Viper
+}
+
+type ImageStoreTypeFlag string
+
+const (
+	FileImageStore ImageStoreTypeFlag = "file"
+	S3ImageStore   ImageStoreTypeFlag = "s3"
+)
+
+func (t *ImageStoreTypeFlag) String() string {
+	return string(*t)
+}
+
+func (t *ImageStoreTypeFlag) Set(v string) error {
+	switch v {
+	case "file", "s3":
+		*t = ImageStoreTypeFlag(v)
+		return nil
+	default:
+		return fmt.Errorf("must be one of %v", []ImageStoreTypeFlag{FileImageStore, S3ImageStore})
+	}
+}
+
+func (t *ImageStoreTypeFlag) Type() string {
+	return "ImageStoreTypeFlag"
 }
--- cmd/run.go
+++ cmd/run.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"context"
 	"errors"
+	"os"
 	"sync"
 
 	"github.com/charmbracelet/log"
@@ -16,13 +17,27 @@ import (
 	"github.com/tifye/x-feed-scraper/storage"
 )
 
-func newRunCommand(cli *cli.CLI) *cobra.Command {
+type runOptions struct {
+	imageStoreType   cli.ImageStoreTypeFlag
+	imageStoreDir    string
+	dbSource         string
+	wipeOnCompletion bool
+}
+
+func newRunCommand(c *cli.CLI) *cobra.Command {
+	opts := runOptions{}
+
 	cmd := &cobra.Command{
 		Use: "run",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return runRun(cmd.Context(), cli.Logger, cli.Config)
+			return runRun(cmd.Context(), c.Logger, c.Config, opts)
 		},
 	}
+
+	cmd.Flags().Var(&opts.imageStoreType, "store", "How image objects are stored")
+	cmd.Flags().StringVar(&opts.imageStoreDir, "dir", "./images", "Where to store image files")
+	cmd.Flags().StringVar(&opts.dbSource, "db", "./state.db", "Sqlite DB source")
+	cmd.Flags().BoolVar(&opts.wipeOnCompletion, "wipe", false, "Clear metadata DB on completion")
 
 	return cmd
 }
@@ -31,20 +46,34 @@ func runRun(
 	ctx context.Context,
 	logger *log.Logger,
 	config *viper.Viper,
+	opts runOptions,
 ) error {
 	wg := sync.WaitGroup{}
 	defer wg.Wait()
 
-	// imgStore, err := storage.FileImageStore("./test")
-	// if err != nil {
-	// 	logger.Fatal(err)
-	// }
-	imgStore, err := storage.NewS3Store(ctx, logger.WithPrefix("s3-store"))
-	if err != nil {
-		logger.Fatal(err)
+	var imgStore storage.ImageStorer
+	switch opts.imageStoreType {
+	case cli.FileImageStore:
+		if opts.imageStoreDir == "" {
+			opts.imageStoreDir = "./images"
+		}
+		is, err := storage.FileImageStore(opts.imageStoreDir)
+		if err != nil {
+			return err
+		}
+		imgStore = storage.ImageStorerFunc(is)
+	default:
+		is, err := storage.NewS3Store(ctx, logger.WithPrefix("s3-store"))
+		if err != nil {
+			return err
+		}
+		imgStore = is
 	}
 
-	imgJobStore, err := storage.NewSqliteImageJobStore(ctx, "./state.db")
+	if opts.dbSource == "" {
+		opts.dbSource = "./state.db"
+	}
+	imgJobStore, err := storage.NewSqliteImageJobStore(ctx, opts.dbSource)
 	if err != nil {
 		logger.Fatal(err)
 	}
@@ -56,6 +85,12 @@ func runRun(
 			logger.Error(err)
 		}
 		logger.Info("closed sqlite job store")
+
+		if opts.wipeOnCompletion {
+			if err := os.Remove(opts.dbSource); err != nil {
+				logger.Errorf("failed to wipe DB: %s", err)
+			}
+		}
 	}()
 
 	ln := launcher.NewUserMode().
